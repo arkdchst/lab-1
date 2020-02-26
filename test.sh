31af1d664ef50ebc3ff30b4f674ebaca4594502d
Arkadij Chistyj <arkdchst@gmail.com>
2020-02-26 20:04:12 +0000

    made bad exit code report

--- test.sh
+++ test.sh
@@ -8,14 +8,20 @@ for i in tests/*.test; do
 
 	cat tests/$i.test | ./out | pcregrep -M "answer:\n([\d ]*\n)*" | pcregrep "^\d" > ans
 
-	echo "answer:"; cat ans
-	echo; echo "right answer:"; cat tests/$i.ans
-
-
-	if [[ "$(cat ans)" == "$(cat tests/$i.ans)" ]]; then
-		echo "TEST PASSED"
+	status="${PIPESTATUS[1]}"
+	if [[ "$status" != "0" ]]; then
+		echo "test NOT passed - exit status "$status
 	else
-		echo "TEST NOT PASSED"
+
+		echo "answer:"; cat ans
+		echo; echo "right answer:"; cat tests/$i.ans
+
+
+		if [[ "$(cat ans)" == "$(cat tests/$i.ans)" ]]; then
+			echo "test passed"
+		else
+			echo "test NOT passed"
+		fi
 	fi
 
 	echo "END OF TEST $i"
